58ecf5315ee78f39c0db6c568616a34e95ecd22f
loic.saillant <loic.saillant.dev@gmail.com>
2025-08-28 14:56:35 +0200

    Initial Commit

---
+++ AppleSampleScrolling/AppleSampleScrollingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleSampleScrollingApp.swift
+//  AppleSampleScrolling
+//
+//  Created by Loïc Saillant on 28/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct AppleSampleScrollingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
